5bd54142726f3e9aa4bbf7983881f9cd7ab3ae0a
김도형 <dohyung97022@gmail.com>
2023-01-04 17:54:50 +0900

    [#5] 내부망 서버 ci, cd용 run.sh build.sh 생성.

--- build.sh
+++ build.sh
@@ -11,6 +11,10 @@ create_requirements_txt() {
   pip freeze >requirements.txt
 }
 
+delete_requirements_txt() {
+  rm requirements.txt
+}
+
 deactivate_venv() {
   deactivate
 }
@@ -24,6 +28,10 @@ zip_file() {
   zip -r appiumProxy.zip .
 }
 
+delete_zip_file() {
+  rm appiumProxy.zip
+}
+
 transfer_zip_file() {
   scp ${PWD}/appiumProxy.zip ${PWD}/run.sh ${SERVER_USERNAME}@${SERVER_IP}:${SERVER_PROJECT_LOCATION}
 }
@@ -33,4 +41,6 @@ create_requirements_txt
 deactivate_venv
 zip_file
 transfer_zip_file
+delete_zip_file
+delete_requirements_txt
 ssh -t ${SERVER_USERNAME}@${SERVER_IP} "sudo ${SERVER_PROJECT_LOCATION}/run.sh"
